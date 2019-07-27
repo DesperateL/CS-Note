0003cdfd23920d21cff6bf074e28da9c6a411ea5
gopher <gopher@Rust.local>
2019-07-27 23:28:14 +0800

    golang label

--- main.go
+++ main.go
@@ -3,22 +3,27 @@ package main
 import "fmt"
 
 func main() {
-label1:
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			fmt.Println(i, j, "--")
-			if i+j > 15 {
-				break label1
-			}
-		}
-	}
+	// label1:
+	// 	for i := 0; i < 10; i++ {
+	// 		for j := 0; j < 10; j++ {
+	// 			fmt.Println(i, j, "--")
+	// 			if i+j > 15 {
+	// 				break label1
+	// 			}
+	// 		}
+	// 	}
+	// 	fmt.Println("break label1")
+	// 	for i := 0; i < 10; i++ {
+	// 		for j := 0; j < 10; j++ {
+	// 			fmt.Println(i, j, "++")
+	// 			if i+j > 15 {
+	// 				goto label1
+	// 			}
+	// 		}
+	// 	}
 	fmt.Println("break label1")
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			fmt.Println(i, j, "++")
-			if i+j > 15 {
-				goto label1
-			}
-		}
-	}
+	goto END
+	fmt.Println("break label2")
+END:
+	fmt.Println("break label3")
 }
