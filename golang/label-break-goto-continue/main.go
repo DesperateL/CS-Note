7aa0e327c22a64407c789e7bec48db11be2e9bdb
梁嘉祺 <i_liangjiaqi@cvte.com>
2019-07-28 01:38:50 +0800

    add

--- golang/label-break-goto-continue/main.go
+++ golang/label-break-goto-continue/main.go
@@ -21,9 +21,25 @@ func main() {
 	// 			}
 	// 		}
 	// 	}
-	fmt.Println("break label1")
-	goto END
-	fmt.Println("break label2")
-END:
-	fmt.Println("break label3")
+	// 	fmt.Println("break label1")
+	// 	goto END
+	// 	fmt.Println("break label2")
+	// END:
+	// 	fmt.Println("break label3")
+FirstLoop:
+	for i := 0; i < 10; i++ {
+		fmt.Println("sadf")
+		for i := 0; i < 10; i++ {
+
+			for i := 0; i < 10; i++ {
+				break FirstLoop
+			}
+		}
+	}
+	goto Block
+	{
+	Block:
+		v := 0
+		fmt.Println(v)
+	}
 }
